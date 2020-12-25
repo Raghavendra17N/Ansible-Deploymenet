6d13a47344415c32fdf07112ef78995dfe11da28
Raghavendra17N <raghava01995@gmail.com>
2020-12-25 04:46:10 +0000

    OS check and Install Ansible

---
+++ K8S/SSH-connection.sh
@@ -0,0 +1,9 @@
+sudo su raghu -c "ansible -m ping -i Inventory/inventory.txt all">/tmp/ssh_logs.txt
+a=`grep -Eo "Failed to connect" /tmp/ssh_logs.txt|uniq`
+while [ "$a" = "Failed to connect" ]
+    do
+        sleep 15
+        sudo su raghu -c "ansible -m ping -i Inventory/inventory.txt all">/tmp/ssh_logs.txt
+        a=`grep -Eo "Failed to connect" /tmp/ssh_logs.txt|uniq`
+    done
+echo "SSH connection Established
