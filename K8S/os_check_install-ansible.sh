46e1098c8e71ea41ea9d7dfcf57c1babef1eb86e
Raghavendra17N <raghava01995@gmail.com>
2020-12-25 04:22:48 +0000

    OS check and Install Ansible

---
+++ K8S/os_check_install-ansible.sh
@@ -0,0 +1,17 @@
+source /etc/os-release
+os_vendor=`echo $ID`
+if [ "$os_vendor" = "centos" ]
+  then
+    sudo yum install epel-release -y 
+	sudo yum install ansible -y 
+elif [ "$os_vendor" = "suse" ]
+  then
+    sudo zypper update
+	sudo zypper install ansible -y 
+else 
+  sudo apt-get update 
+  sudo apt install ansible -y 
+fi
+mkdir -p /etc/ansible/group_vars
+sudo cp Inventory/group_vars/k8s_master.txt /etc/ansible/group_vars/k8s_master
+sudo cp Inventory/group_vars/k8s_worker.txt /etc/ansible/group_vars/k8s_worker
