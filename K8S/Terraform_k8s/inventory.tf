28569b20a308d4214c711194c715c93ada2f1a39
Raghavendra17N <45710500+Raghavendra17N@users.noreply.github.com>
2020-12-26 14:48:01 +0530

    Update inventory.tf

--- K8S/Terraform_k8s/inventory.tf
+++ K8S/Terraform_k8s/inventory.tf
@@ -1,34 +1,39 @@
-resource "null_resource" "entry" {
+resource "null_resource" "empty_inv"{
+    provisioner "local-exec"{
+        command = "cat /dev/null > ../Inventory/inventory.txt"
+    }
+}
+resource "null_resource" "master-grp" {
     triggers = {
-        order = "${google_compute_instance.test-1-vm.name}"
+        order = "${google_compute_instance.master-k8s.name}"
     }
     provisioner "local-exec"{
-       command = "echo [test_hosts1] >> inventory"
+       command = "echo [k8s_master] >> inventory"
    }
 }
-resource "null_resource" "test1"{
+resource "null_resource" "master-ip"{
     triggers = {
-        order = "${null_resource.entry.id}"
+        order = "${null_resource.master-grp.id}"
     }
     provisioner "local-exec"{
 
-        command = "echo ${google_compute_instance.test-1-vm.name} ansible_host=${google_compute_instance.test-1-vm.network_interface.0.access_config.0.nat_ip} >> "
+        command = "echo ${google_compute_instance.master-k8s.name} ansible_host=${google_compute_instance.master-k8s.network_interface.0.access_config.0.nat_ip} >> ../Inventory/inventory.txt"
     }
 }
-resource "null_resource" "entry2" {
+resource "null_resource" "worker-grp" {
     triggers = {
-        order = "${null_resource.test1.id}"
+        order = "${null_resource.master-ip.id}"
     }
     provisioner "local-exec"{
-       command = "echo [test_hosts2] >> inventory"
+       command = "echo [k8s_worker] >> ../Inventory/inventory.txt"
    }
 }
-resource "null_resource" "test2"{
+resource "null_resource" "worker-ip"{
     triggers = {
-        order = "${null_resource.entry2.id}"
+        order = "${null_resource.worker-grp.id}"
     }
     provisioner "local-exec"{
 
-        command = "echo ${google_compute_instance.test-2-vm.name} ansible_host=${google_compute_instance.test-2-vm.network_interface.0.access_config.0.nat_ip} >>inventory"
+        command = "echo ${google_compute_instance.node-k8s.name} ansible_host=${google_compute_instance.node-k8s.network_interface.0.access_config.0.nat_ip} >> ../Inventory/inventory.txt"
     }
 }
